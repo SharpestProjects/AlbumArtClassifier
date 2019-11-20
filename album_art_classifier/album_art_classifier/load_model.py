c8c71b9e4c5fbe24b2b883dbcb8f53a8254f10e2
russell-pollari <pollarir@mgail.com>
2019-11-20 18:51:34 -0500

    Fix linting errors

--- album_art_classifier/album_art_classifier/load_model.py
+++ album_art_classifier/album_art_classifier/load_model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import load_model
 
 # TODO: Load model from remote storage
-print('Loading model...');
+print('Loading model...')
 model = load_model('trained_models/model.h5')
--- album_art_classifier/album_art_classifier/predict_model.py
+++ album_art_classifier/album_art_classifier/predict_model.py
@@ -1,19 +1,19 @@
 import numpy as np
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
+
 def predict_model(model, file):
 	try:
 		x = img_to_array(load_img(file)) / 256
-	except:
+	except: # noqa E722
 		return 'Invalid input', 400
 
 	# TODO: validate input
 	pred = model.predict(np.expand_dims(x, axis=0))
 
 	print('Input', x)
-	print('Output', pred);
+	print('Output', pred)
 
-	result = pred[0][0]
 	genre = 'metal!' if pred[0][0] < 0.5 else 'not metal'
 
 	return genre
