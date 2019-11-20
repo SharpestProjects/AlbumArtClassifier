2722f0ab0bc63d4c29d42321c4a6a9cf5248a37b
russell-pollari <pollarir@mgail.com>
2019-11-20 18:21:33 -0500

    Package for machine learning model

---
+++ album_art_classifier/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+	name="album_art_classifier",
+	version="0.1.0",
+	packages=['album_art_classifier']
+)
--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
 import numpy as np
 from flask import Flask, request, render_template, jsonify
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-# TODO: Load model from remote storage
-print('Loading model...');
-model = load_model('trained_models/model.h5')
+from album_art_classifier.predict_model import predict_model
+from album_art_classifier.load_model import model
 
 app = Flask(__name__)
 
@@ -23,19 +20,7 @@ def predict():
 
 		file = request.files['album']
 		# TODO: check file type and format etc
-		try:
-			x = img_to_array(load_img(file)) / 256
-		except:
-			return 'Invalid input', 400
-
-		# TODO: validate input
-		pred = model.predict(np.expand_dims(x, axis=0))
-
-		print('Input', x)
-		print('Output', pred);
-
-		result = pred[0][0]
-		genre = 'metal!' if pred[0][0] < 0.5 else 'not metal'
+		genre = predict_model(model, file)
 
 		return jsonify({ 'genre': genre });
 
