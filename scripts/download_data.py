b9256acd0d0014d92a999416450b6685b533488a
russell-pollari <pollarir@mgail.com>
2019-12-04 17:59:45 -0500

    Add download script

---
+++ scripts/download_data.py
@@ -0,0 +1,18 @@
+import wget
+
+import tempfile
+import tarfile
+
+
+def download_dataset(data_url):
+	with tempfile.TemporaryDirectory() as tmpdir:
+		tmp_path = '{}/data.tar.gz'.format(tmpdir)
+		wget.download(data_url, tmp_path)
+
+		with tarfile.open(tmp_path) as tar:
+			tar.extractall()
+
+
+if __name__ == '__main__':
+	data_url = 'https://album-art-classifier.s3-us-west-2.amazonaws.com/album-art.tar.gz'  # noqa: E501
+	download_dataset(data_url)
